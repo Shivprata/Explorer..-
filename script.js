745926ee21fb0cd9f59815e5e41d6590a15e7a3d
Shivpratap <shivpratapk51@gamil.com>
2023-12-18 21:13:56 +0530

    my changes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatbotToggler = document.querySelector(".chatbot-toggler");
 const chatbotCloseBtn = document.querySelector(".close-btn");
 
 let usermessege;
-const API_KEY ="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";
+const API_KEY ="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";
 const inputInitHeight = chatInput.scrollHeight;
 
 
